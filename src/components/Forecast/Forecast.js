e31326a8cb73460bdfdbfba411ae21a3eddd3d45
Shardul Vaidya <cam.v737@gmail.com>
2021-01-28 15:36:31 -0500

    Regularising the look of the application using react-bootstrap

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,6 +1,10 @@
 import Conditions from '../Conditions/Conditions';
 import classes from './Forecast.module.css';
+
 import React, { useState } from 'react';
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 let cities = [];
 
@@ -65,38 +69,29 @@ const Forecast = () => {
         // JSX code
         <div>
             <h2>Find Current Weather Conditions</h2>
-            <form onSubmit={getForecast}>
-                <input
+            <Form onSubmit={getForecast}>
+                <Form.Control
                     type="text"
                     placeholder="Enter City"
-                    maxLength="50"
-                    className={classes.textInput}
                     value={city}
                     onChange={(e) => setCity(e.target.value)}
+                />
+                <Form.Check 
+                    type='radio' 
+                    label='Farenheit'
+                    value="imperial"
+                    onChange={(e) => setUnit(e.target.value)}
+                    checked={unit === "imperial"}
                     />
-                <label className={classes.Radio}>
-                    <input
-                        type="radio"
-                        name="units"
-                        checked={unit === "imperial"}
-                        value="imperial"
-                        onChange={(e) => setUnit(e.target.value)}
-                        />
-                    Fahrenheit
-                </label>
-                <label className={classes.Radio}>
-                    <input
-                        type="radio"
-                        name="units"
-                        checked={unit === "metric"}
-                        value="metric"
-                        onChange={(e) => setUnit(e.target.value)}
-                        />
-                    Celcius
-                </label>
-
-                <button className={classes.Button} type="submit">Get Weather and Forecast</button>
-            </form>
+                <Form.Check
+                    type='radio'
+                    label='Celsius'
+                    value="metric"
+                    onChange={(e) => setUnit(e.target.value)}
+                    checked={unit === "metric"}
+                    />
+                <Button className={classes.Button} type="submit">Get Weather and Forecast</Button>
+            </Form>
             <Conditions
                responseObj={responseObj}
                error={error}
