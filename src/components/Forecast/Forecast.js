716aabd39fabdfdd2545855198c06f1a07c09aa0
Shardul Vaidya <cam.v737@gmail.com>
2021-01-29 00:57:28 -0500

    Updated UI to Material Spec

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -3,8 +3,19 @@ import classes from './Forecast.module.css';
 
 import React, { useState } from 'react';
 
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import {FormControl, FormLabel, Radio, RadioGroup} from "@material-ui/core";
 
 let cities = [];
 
@@ -67,39 +78,50 @@ const Forecast = () => {
 
     return (
         // JSX code
-        <div>
-            <h2>Find Current Weather Conditions</h2>
-            <Form onSubmit={getForecast}>
-                <Form.Control
-                    type="text"
-                    placeholder="Enter City"
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
+        <Container component={"main"} maxWidth={"xs"}>
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Typography component={"h1"} variant={"h5"}>
+                    Find Current Weather Conditions
+                </Typography>
+                <form className={classes.form} noValidate onSubmit={getForecast}>
+                    <TextField
+                        variant={"outlined"}
+                        margin={"normal"}
+                        required
+                        fullWidth
+                        id={city}
+                        label={"City"}
+                        name={"city"}
+                        placeholder={"Enter City"}
+                        autoFocus
+                        onChange={(e) => setCity(e.target.value)}
+                        />
+                        <FormControl component={"fieldset"}>
+                            <FormLabel component={"units"}/>
+                            <RadioGroup aria-label={"units"} name={"units"} value={unit} onChange={setUnit}>
+                                <FormControlLabel value={"metric"} control={<Radio/>} label={"Celsius"}/>
+                                <FormControlLabel value={"imperial"} control={<Radio/>} label={"Fahrenheit"}/>
+                            </RadioGroup>
+                        </FormControl>
+                    <Button
+                        type={"submit"}
+                        fullWidth
+                        variant={"contained"}
+                        color={"primary"}
+                        className={classes.submit}>
+                        Get Weather
+                    </Button>
+                </form>
+                <Conditions
+                    responseObj={responseObj}
+                    error={error}
+                    loading={loading}
+                    exists={seen}
+                    cities={weatherList}
                 />
-                <Form.Check 
-                    type='radio' 
-                    label='Farenheit'
-                    value="imperial"
-                    onChange={(e) => setUnit(e.target.value)}
-                    checked={unit === "imperial"}
-                    />
-                <Form.Check
-                    type='radio'
-                    label='Celsius'
-                    value="metric"
-                    onChange={(e) => setUnit(e.target.value)}
-                    checked={unit === "metric"}
-                    />
-                <Button className={classes.Button} type="submit">Get Weather and Forecast</Button>
-            </Form>
-            <Conditions
-               responseObj={responseObj}
-               error={error}
-               loading={loading}
-               exists={seen}
-               cities={weatherList}
-               />
-        </div>
+            </div>
+        </Container>
     )
 }
 
